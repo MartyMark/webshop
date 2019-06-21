5775f76b70d6e63178bc0ad1d8f85c8594326d34
Tim <tim.busanny@gmail.com>
2019-06-21 19:40:43 +0200

    static image path hinzugefügt, sollten nun korrekt geladen werden

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,20 @@
-module.exports.submit = function(req, res) {
+module.exports.submit = function (req, res) {
     var email = req.body.email;
     var password = req.body.psw;
-    let ip = req.connection.remoteAddress
+    let ip = req.connection.remoteAddress;
 
     var sql = "SELECT * FROM user WHERE email = '" + email + "' and password = '" + password + "'";
 
-    console.log(sql)
+    console.log(sql);
 
-    global.connection.query(sql, function(err, result) {
+    global.connection.query(sql, function (err, result) {
         if (err) throw err;
 
         if (!result.length) {
             res.redirect('/login');
             return;
         }
-        let userid = result[0].id
+        let userid = result[0].id;
 
         global.userCache.set(ip, userid);
 
